f8ee24f958bbb00881f1efb5cb11c5bc198882bd
TmLololol <tmtanky@gmail.com>
2022-08-28 00:19:01 +0800

    feat: initial setup of working algolia search
    alg/adding-algolia-packages-setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,60 @@
-import React, { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
+import React from 'react';
+import algoliasearch from 'algoliasearch/lite';
+import {
+  InstantSearch,
+  SearchBox,
+  Hits,
+  Highlight,
+} from 'react-instantsearch-hooks-web';
+import type { Hit, BaseHit } from 'instantsearch.js';
 
-function App() {
-  const [count, setCount] = useState(0);
+const searchClient = algoliasearch(
+  import.meta.env.VITE_ALGOLIA_APP_ID,
+  import.meta.env.VITE_ALGOLIA_SEARCH_ID
+);
+
+interface Product {
+  objectID: string;
+  name: string;
+  description: string;
+  brand: string;
+  categories: string[];
+  price: number;
+  image: string;
+  popularity: number;
+}
+
+function CustomHit({ hit }: { hit: Hit<BaseHit> }) {
+  const item = hit as unknown as Product;
 
   return (
+    <article>
+      <img src={item.image} alt={item.name} />
+      <p>{item.categories[0]}</p>
+      <h2>
+        <Highlight attribute='name' hit={hit} />
+      </h2>
+      <p>${item.price}</p>
+    </article>
+  );
+}
+
+function Search() {
+  return (
+    <>
+      <h1> Algolia Practice </h1>
+      <SearchBox />
+      <Hits hitComponent={CustomHit} />
+    </>
+  );
+}
+
+function App() {
+  return (
     <div className='App'>
-      <div>
-        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-          <img src='/vite.svg' className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://reactjs.org' target='_blank' rel='noreferrer'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button type='button' onClick={() => setCount((state) => state + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
+      <InstantSearch searchClient={searchClient} indexName='search-sample'>
+        <Search />
+      </InstantSearch>
     </div>
   );
 }
